552613290bc417fe1066d3b0b7fc211c258a73f3
Tiggger <johnnyalexjw@gmail.com>
2026-02-13 10:52:26 +0000

    Measurements methods implemented and can plot a distribution of absorption times in linear and log space. Also implemented a glider starting condition.

--- CP2/cp2.py
+++ CP2/cp2.py
@@ -9,8 +9,10 @@ import csv
 plt.style.use('shendrukGroupStyle')
 import shendrukGroupFormat as ed
 
-
+#################
 #Args
+#################
+
 parser = argparse.ArgumentParser(
     prog='ProgramName',
     description='Code for Checkpoint 1 of MVP',
@@ -26,13 +28,21 @@ parser.add_argument('-r', '--random', type=int, default='1',
 parser.add_argument('-s', '--startingCondition', type=str, default=None,
                     help='The starting condition that you want to try options include: {Need to add options once implemented}')
 
-parser.add_argument('-p', '--pAlive', type=float, default=None,
+parser.add_argument('-p', '--pAlive', type=float, default=0.5,
                     help='The probability that a cell is alive upon intialisation')
 
 parser.add_argument('-g', '--graphics', type=int, default='1',
                     help='Graphics mode (visualisation), 0 for off, 1 for on')
 
+parser.add_argument('-d', '--debug', type=int, default='0',
+                    help='Debug mode (prints a lot of things to help), 0 for off, 1 for on. Default is off')
+
 args = parser.parse_args()
+
+
+#################
+# Functions
+#################
 
 #Conway procedure to update grid
 def conwayProcedure(grid):
@@ -76,7 +86,10 @@ def conwayProcedure(grid):
     #print(newGrid, 'newGrid')
     return newGrid
 
+#################
 #Plotting Functions
+#################
+
 #Initialse video plotter
 def init_plot(grid, title='Ising Dynamics'):
     """Initialize interactive plot."""
@@ -99,7 +112,22 @@ def update_plot(im, fig, grid):
     fig.canvas.flush_events()
     plt.pause(0.001)
 
+
+#################
+#Measurement functions
+#################
+
+
+#function to measure number of active sites
+def measureActiveSites(grid):
+    unique, counts = np.unique(grid, return_counts=True)
+
+    return counts[1]
+
+#################
 # Error Function - Bootstrapping
+#################
+
 def bootstrap(data, function, *args, n_bootstrap=1000):
     """Bootstrap error estimation"""
     bootstrap_values = []
@@ -114,32 +142,51 @@ def bootstrap(data, function, *args, n_bootstrap=1000):
     # The standard deviation of bootstrap distribution is the error estimate
     return np.std(bootstrap_values)
 
+
+#################
 # Simulation Parameters
+#################
 
 #put in variables like this as numba doesn't like when you pass args.X in
 L = args.n
 sweep = L * L
 pAlive=args.pAlive
 
-#carried over from checkpoint 1
-n_warm = 25000
-n_meas = 15000
+#for measuring absorption times
+nMeas = 1000
+absorptionTimes=[]
+simulationAbsorped=False
 
 #lists to store data
 
 #some arbitrary number of steps that is large, such that when visualising, it plays for a long time
 visualiserSteps = 10_000_000_000_000
-sweepCounter=0
+updateCounter=0
 
 # ------------------------------
 # Main loop
 # ------------------------------
 
-#initialising grid
-grid = np.random.choice(np.array([1, 0], dtype=np.int8), size=(L, L), p=[pAlive, 1-pAlive])
+
 
 #graphics mode
 if args.graphics==1:
+
+    #initialise grid
+    if args.random==1:
+        #initialising grid
+        grid = np.random.choice(np.array([1, 0], dtype=np.int8), size=(L, L), p=[pAlive, 1-pAlive])
+    #glider
+    elif args.random==0 and args.startingCondition.lower()=='glider':
+        grid = np.zeros((L, L), dtype=np.int8)
+        
+        glider = np.array([[0, 1, 0],
+                       [0, 0, 1],
+                       [1, 1, 1]], dtype=np.int8)
+        
+        start_y = L // 2 - 1
+        start_x = L // 2 - 1
+        grid[start_y:start_y+3, start_x:start_x+3] = glider
 
     #initialise figure for plotting
     fig, ax, im = init_plot(grid, "Conway's Game of Life")
@@ -154,6 +201,54 @@ if args.graphics==1:
 #non grpahics mode
 elif args.graphics==0:
 
-    for step in range(visualiserSteps):
-        grid = conwayProcedure(grid)
-        print(grid, 'grid')
+    #repeat simulation for the number of measurements that we want
+    for sim in range(nMeas):
+        print(sim)
+
+        #initialise random grid
+        grid = np.random.choice(np.array([1, 0], dtype=np.int8), size=(L, L), p=[pAlive, 1-pAlive])
+        dAcounter=0
+
+        #simulate for a long amount of time
+        for step in range(visualiserSteps):
+
+            #get number of active sites before we update
+            activeSitesBefore=measureActiveSites(grid)
+
+            #do conway for the grid
+            grid = conwayProcedure(grid)
+
+            #get the number of active sites again after we update
+            activeSitesAfter=measureActiveSites(grid)
+
+            dA=activeSitesAfter-activeSitesBefore
+
+            if dA==0:
+                dAcounter+=1
+
+            #debug condition
+            if args.debug:
+                #print(grid, 'grid')
+                print('step: ', step, 'change in active sites', dA)
+
+            #if we have detected that the simulation has been absorped, then reset the grid
+            if dAcounter>=5:
+                absorptionTimes.append(step)
+                break
+
+    with open('absorptionTimes.csv', 'w', newline='') as f:
+        writer=csv.writer(f)
+        writer.writerow(['Data in rows as: absorptionTimes'])
+        writer.writerow(absorptionTimes)
+    
+    #linear scale
+    plt.xlabel(r'$\tau_\mathrm{Absorption}$')
+    plt.ylabel(r'$p(\tau_\mathrm{Absorption})$')
+    plt.hist(absorptionTimes, density=True, bins=20, log=False)
+    plt.show()
+
+    #log scale
+    plt.xlabel(r'$\tau_\mathrm{Absorption}$')
+    plt.ylabel(r'$p(\tau_\mathrm{Absorption})$')
+    plt.hist(absorptionTimes, density=True, bins=20, log=True)
+    plt.show()
