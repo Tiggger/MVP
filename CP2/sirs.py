6d017aa18c2f48f0f35a5b76001010afcbb98bf6
Tiggger <johnnyalexjw@gmail.com>
2026-03-01 13:08:56 +0000

    Finished CP2, saving final results and updates

--- CP2/sirs.py
+++ CP2/sirs.py
@@ -28,9 +28,6 @@ parser.add_argument('-n', '--n', type=int, default=50,
 parser.add_argument('-r', '--random', type=int, default='1',
                     help='Whether you want a random initial condition or not, by default it is turned on')
 
-parser.add_argument('-s', '--startingCondition', type=str, default=None,
-                    help='The starting condition that you want to try options include: {Need to add options once implemented}')
-
 parser.add_argument('-p', '--pAlive', type=float, default=0.5,
                     help='The probability that a cell is alive upon intialisation')
 
@@ -48,6 +45,13 @@ parser.add_argument('-pir', '--pir', type=float, default=0.5,
 
 parser.add_argument('-prs', '--prs', type=float, default=0.5,
                     help='Probability of a recovered becoming susceptible again. Default is 0.5')
+
+parser.add_argument('-m', '--measurementMode', type=int, default=0,
+                    help='Defines what type of measurement you want to make. 0 - heatmap for varying psi and prs, pir=0.5. 1 - contour plot of variance in infected for prs=0.5, psi=0.2-0.5. Default is 0.')
+
+parser.add_argument('-if', '--immunityFraction', type=float, default=0,
+                    help='Defines the fraction of agents that are immune. Please note that immunity is permanent. Default is 0')
+
 
 
 args = parser.parse_args()
@@ -129,10 +133,13 @@ def update_plot(im, fig, grid, sweep_count=None, ax_pop=None, lines=None, histor
 
 #sirs update function
 @njit
-def doSirsUpdate(L, grid, psi, pir, prs):
+def doSirsUpdate(L, grid, psi, pir, prs, immunityMatrix=None):
     #pick a site
     x = np.random.randint(0, L)
     y = np.random.randint(0, L)
+
+    if immunityMatrix is not None:
+        immunityStatus=immunityMatrix[x, y]
 
     #get value and list of neighbours
     candidate = grid[x, y]
@@ -154,10 +161,36 @@ def doSirsUpdate(L, grid, psi, pir, prs):
 
     #if candidate is recovered
     elif candidate==1:
-        if np.random.rand()<prs:
-            grid[x,y]=-1
+        if immunityMatrix is not None:
+            if immunityStatus==0:
+                if np.random.rand()<prs:
+                    grid[x,y]=-1
+            # else:
+            #     print('Theyre immune!')
+        else:
+            if np.random.rand()<prs:
+                grid[x,y]=-1
 
-#average infected calculation function
+
+#bootstrap error function
+def bootstrap(data, function, *args, n_bootstrap=1000):
+    """Bootstrap error estimation"""
+    bootstrap_values = []
+    
+    for i in range(n_bootstrap):
+        # Resample with replacement
+        samples = np.random.choice(data, size=len(data), replace=True)
+        # Calculate statistic on resampled data
+        value = function(samples, *args)
+        bootstrap_values.append(value)
+    
+    # The standard deviation of bootstrap distribution is the error estimate
+    return np.std(bootstrap_values)
+
+
+#function to calculate variance
+def calcVar(data, N):
+    return (np.average(data**2)-np.average(data)**2)/N
 
 
 #Arguments
@@ -173,8 +206,13 @@ debug=args.debug
 
 #for phase space diagram maker
 res=25
-numMeas=1000 #low for testing
+numMeas=1000
 avgInfectedResults=np.zeros([res, res])
+
+infectedVar=np.zeros(res)
+infectedVarError=np.zeros(res)
+
+avgInfectedFracImmuneResults=np.zeros(res)
 
 
 
@@ -183,65 +221,169 @@ if args.graphics==1:
 
     #initialise grid
     if args.random==1:
-        #initialising grid - conventional choice that S=-1, I=0, R=1
-        grid = np.random.choice(np.array([-1, 0, 1], dtype=np.int8), size=(L, L))
 
-        #initialise figure for plotting
-        fig, ax, im, _, _, _ = init_plot(grid, "SIRS", show_populations=False)
+        if args.immunityFraction == 0:
+            #initialising grid - conventional choice that S=-1, I=0, R=1
+            grid = np.random.choice(np.array([-1, 0, 1], dtype=np.int8), size=(L, L))
 
-        for step in range(visualiserSteps):
+            #initialise figure for plotting
+            fig, ax, im, _, _, _ = init_plot(grid, "SIRS", show_populations=False)
 
-            #do sirs update 
-            doSirsUpdate(L, grid, psi, pir, prs)
+            for step in range(visualiserSteps):
 
-            #only update grid after a sweep
-            if (step + 1) % sweep == 0 or step==visualiserSteps-1:
+                #do sirs update 
+                doSirsUpdate(L, grid, psi, pir, prs)
 
-                #visually update the grid
-                update_plot(im, fig, grid)
+                #only update grid after a sweep
+                if (step + 1) % sweep == 0 or step==visualiserSteps-1:
+
+                    #visually update the grid
+                    update_plot(im, fig, grid)
+        
+        elif args.immunityFraction != 0:
+            print('Immunity mode running')
+            grid = np.random.choice(np.array([-1, 0, 1], dtype=np.int8), size=(L, L))
+
+            immunityMatrix = np.random.choice(np.array([True, False], dtype=np.int8), size=(L, L), p=[args.immunityFraction, 1-args.immunityFraction])
+
+            #initialise figure for plotting
+            fig, ax, im, _, _, _ = init_plot(grid, "SIRS", show_populations=False)
+
+            for step in range(visualiserSteps):
+
+                #do sirs update 
+                doSirsUpdate(L, grid, psi, pir, prs, immunityMatrix=immunityMatrix)
+
+                #only update grid after a sweep
+                if (step + 1) % sweep == 0 or step==visualiserSteps-1:
+
+                    #visually update the grid
+                    update_plot(im, fig, grid)
 
 #graphics mode to plot 
 elif args.graphics==2:
     #initialise grid
     if args.random==1:
-        #initialising grid - conventional choice that S=-1, I=0, R=1
-        grid = np.random.choice(np.array([-1, 0, 1], dtype=np.int8), size=(L, L))
 
-        #initialise figure for plotting
-        fig, ax, im, ax_pop, lines, histories = init_plot(grid, "SIRS", show_populations=True)
+        if args.immunityFraction == 0:
+            #initialising grid - conventional choice that S=-1, I=0, R=1
+            grid = np.random.choice(np.array([-1, 0, 1], dtype=np.int8), size=(L, L))
 
-        for step in range(visualiserSteps):
+            #initialise figure for plotting
+            fig, ax, im, ax_pop, lines, histories = init_plot(grid, "SIRS", show_populations=True)
 
-            #do sirs update 
-            doSirsUpdate(L, grid, psi, pir, prs)
+            for step in range(visualiserSteps):
 
-            #only update grid after a sweep
-            if (step + 1) % sweep == 0 or step==visualiserSteps-1:
-                sweep_count = (step + 1) // sweep
+                #do sirs update 
+                doSirsUpdate(L, grid, psi, pir, prs)
 
-                #visually update the grid
-                update_plot(im, fig, grid, sweep_count=sweep_count, ax_pop=ax_pop, lines=lines, histories=histories)
+                #only update grid after a sweep
+                if (step + 1) % sweep == 0 or step==visualiserSteps-1:
+                    sweep_count = (step + 1) // sweep
+
+                    #visually update the grid
+                    update_plot(im, fig, grid, sweep_count=sweep_count, ax_pop=ax_pop, lines=lines, histories=histories)
+
+        if args.immunityFraction != 0:
+            #initialising grid - conventional choice that S=-1, I=0, R=1
+            grid = np.random.choice(np.array([-1, 0, 1], dtype=np.int8), size=(L, L))
+
+            immunityMatrix = np.random.choice(np.array([True, False], dtype=np.int8), size=(L, L), p=[args.immunityFraction, 1-args.immunityFraction])
+
+            #initialise figure for plotting
+            fig, ax, im, ax_pop, lines, histories = init_plot(grid, "SIRS", show_populations=True)
+
+            for step in range(visualiserSteps):
+
+                #do sirs update 
+                doSirsUpdate(L, grid, psi, pir, prs, immunityMatrix=immunityMatrix)
+
+                #only update grid after a sweep
+                if (step + 1) % sweep == 0 or step==visualiserSteps-1:
+                    sweep_count = (step + 1) // sweep
+
+                    #visually update the grid
+                    update_plot(im, fig, grid, sweep_count=sweep_count, ax_pop=ax_pop, lines=lines, histories=histories)
+
 
 
 #measurement mode
-
-
-
 elif args.graphics==0:
     if args.random==1:
 
-        #set linspaces for psi and prs
-        psiVals=np.linspace(0, 1, res)
-        prsVals=np.linspace(0, 1, res)
+        if args.measurementMode==0:
 
-        #looping through parameter set
-        for psi in range(res):
-            for prs in range(res):
+            print('Measuring phase space')
+
+            #set linspaces for psi and prs
+            psiVals=np.linspace(0, 1, res)
+            prsVals=np.linspace(0, 1, res)
+
+            #looping through parameter set
+            for psi in range(res):
+                for prs in range(res):
+                    
+                    #do 1 simulation, but measure 1000 times
+
+                    if debug:
+                        print(f'Doing sim with psi={psiVals[psi]} and prs={prsVals[prs]}')
+                    
+                    #initialise random grid
+                    grid = np.random.choice(np.array([-1, 0, 1], dtype=np.int8), size=(L, L))
+
+                    #initialise list to save results for this parameter set
+                    infectedResults=[]
+
+                    #warm up for 100 sweeps
+                    for _ in range(100*sweep):
+                        doSirsUpdate(L, grid, psiVals[psi], 0.5, prsVals[prs]) #hard code pir as 0.5
+                    
+                    #measurements for 1000 sweeps
+                    for _ in range(1000*sweep):
+                        #do update
+                        doSirsUpdate(L, grid, psiVals[psi], 0.5, prsVals[prs])
+
+                        #measure infected
+                        infectedResults.append(np.count_nonzero(grid==0))
+                    
+                    #take average of infected results
+                    avgInfected=np.average(infectedResults)
+
+                    #normalise by grid size
+                    avgInfected/=(L*L)
+
+                    # print([psi, prs])
+                    avgInfectedResults[psi, prs]=avgInfected
+
+            #save results
+            with open('phaseSpace.csv', 'w', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow(['psi_index', 'prs_index', 'avgInfected'])  # header
+                for psi in range(res):
+                    for prs in range(res):
+                        writer.writerow([psi, prs, avgInfectedResults[psi, prs]])
+
+            #plot results
+            plt.imshow(avgInfectedResults)
+            plt.grid(False)
+            plt.colorbar()
+            plt.xlabel(r'$P_{S \rightarrow I}$')
+            plt.ylabel(r"$P_{R \rightarrow S}$")
+            plt.title(r"$\langle I \rangle / N$")
+            plt.show()
+    
+        elif args.measurementMode==1:
+            print('Measuring variance')
+
+            #set linspaces for psi and prs
+            psiVals=np.linspace(0.2, 0.5, res)
+            prsVals=0.5
+
+            #looping through parameter set
+            for psi in range(res):
                 
-                #do 1 simulation, but measure 1000 times
-
                 if debug:
-                    print(f'Doing sim with psi={psiVals[psi]} and prs={prsVals[prs]}')
+                    print(f'Doing sim with psi={psiVals[psi]}')
                 
                 #initialise random grid
                 grid = np.random.choice(np.array([-1, 0, 1], dtype=np.int8), size=(L, L))
@@ -251,14 +393,63 @@ elif args.graphics==0:
 
                 #warm up for 100 sweeps
                 for _ in range(100*sweep):
-                    doSirsUpdate(L, grid, psiVals[psi], 0.5, prsVals[prs]) #hard code pir as 0.5
+                    doSirsUpdate(L, grid, psiVals[psi], 0.5, 0.5) #hard code pir as 0.5
                 
-                #measurements for 1000 sweeps
-                for _ in range(1000*sweep):
+                #measurements for 10000 sweeps
+                for _ in range(100*sweep):
                     #do update
-                    doSirsUpdate(L, grid, psiVals[psi], 0.5, prsVals[prs])
+                    doSirsUpdate(L, grid, psiVals[psi], 0.5, 0.5)
 
                     #measure infected
+                    infectedResults.append(np.count_nonzero(grid==0))
+                    
+                var=calcVar(np.array(infectedResults), L*L)
+
+                error=bootstrap(infectedResults, calcVar, L*L, n_bootstrap=100)
+
+                # print([psi, prs])
+                infectedVar[psi]=var
+                infectedVarError[psi]=error
+            
+            #need to handle error
+
+            with open('varResults.csv', 'w', newline='') as f:
+                writer=csv.writer(f)
+                writer.writerow(['Data in rows as: Variance, error on variance'])
+                writer.writerow(infectedVar)
+                writer.writerow(infectedVarError)
+            
+            plt.plot(psiVals, infectedVar)
+            plt.fill_between(psiVals, infectedVar-infectedVarError, infectedVar+infectedVarError, label='Bootstrap')
+            plt.xlabel(r'$P_{S\rightarrow I}$')
+            plt.ylabel(r'$\frac{\langle I^2\rangle-\langle I\rangle^2}{N}$')
+            plt.show()
+
+        #looking at the fraction of immune
+        elif args.measurementMode == 2:
+            print('Measuring Infections as function of fraction of immune')
+
+            fracsImmune=np.linspace(0, 1, res)
+
+            for frac in range(res):
+
+                if debug:
+                    print(f'Simulating with immunity fraction {fracsImmune[frac]}')
+
+                grid = np.random.choice(np.array([-1, 0, 1], dtype=np.int8), size=(L, L))
+
+                immunityMatrix = np.random.choice(np.array([True, False], dtype=np.int8), size=(L, L), p=[fracsImmune[frac], 1-fracsImmune[frac]])
+
+                infectedResults=[]
+
+
+                #warm up for 100 sweeps - not sure if we want to do warm up with immunity or not
+                for _ in range(100*sweep):
+                    doSirsUpdate(L, grid, 0.5, 0.5, 0.5)
+
+                for _ in range(1000*sweep):
+                    doSirsUpdate(L, grid, 0.5, 0.5, 0.5, immunityMatrix=immunityMatrix)
+
                     infectedResults.append(np.count_nonzero(grid==0))
                 
                 #take average of infected results
@@ -268,10 +459,17 @@ elif args.graphics==0:
                 avgInfected/=(L*L)
 
                 # print([psi, prs])
-                avgInfectedResults[psi, prs]=avgInfected
+                avgInfectedFracImmuneResults[frac]=avgInfected
+            
+            with open('fracImmuneResults.csv', 'w', newline='') as f:
+                writer=csv.writer(f)
+                writer.writerow(['Data in rows as: frac immune, average infected'])
+                writer.writerow(fracsImmune)
+                writer.writerow(avgInfectedFracImmuneResults)
+            
+            plt.plot(fracsImmune, avgInfectedFracImmuneResults)
+            plt.xlabel(r'$f_\mathrm{Im}$')
+            plt.ylabel(r'$\langle I\rangle/N$')
+            plt.show()
 
-    
-    plt.imshow(avgInfectedResults)
-    plt.grid(False)
-    plt.colorbar()
-    plt.show()
+
